382876b2212636fc3d4113b317ed712782bd2f8d
Andre LeBlanc <andrepleblanc@gmail.com>
2013-05-16 17:33:54 -0400

    added missing file

---
+++ src/main/java/com/norcode/bukkit/buildinabox/BlockProtectionListener.java
@@ -0,0 +1,76 @@
+package com.norcode.bukkit.buildinabox;
+
+import java.util.Iterator;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public class BlockProtectionListener implements Listener {
+
+    @EventHandler(ignoreCancelled=true)
+    public void onBlockBreak(final BlockBreakEvent event) {
+        if (event.getBlock().hasMetadata("biab-block") || (event.getBlock().getType().equals(Material.ENDER_CHEST) && event.getBlock().hasMetadata("buildInABox"))) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled=true)
+    public void onExplosion(EntityExplodeEvent event) {
+        Iterator<Block> it = event.blockList().iterator();
+        Block b;
+        while (it.hasNext()) {
+            b = it.next();
+            if (b.hasMetadata("biab-block") || b.hasMetadata("buildInABox")) {
+                it.remove();
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled=true)
+    public void onBlockBurn(BlockBurnEvent event) {
+        if (event.getBlock().hasMetadata("biab-block") || event.getBlock().hasMetadata("buildInABox")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled=true)
+    public void onBlockSpread(BlockSpreadEvent event) {
+        if (event.getBlock().hasMetadata("biab-block") || event.getBlock().hasMetadata("buildInABox")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled=true)
+    public void onBlockFade(BlockFadeEvent event) {
+        if (event.getBlock().hasMetadata("biab-block") || event.getBlock().hasMetadata("buildInABox")) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled=true)
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        for (Block b: event.getBlocks()) {
+            if (b.hasMetadata("biab-block") || b.hasMetadata("buildInABox")) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled=true)
+    public void onPistonRetract(BlockPistonRetractEvent event) {
+        Block b = event.getRetractLocation().getBlock();
+        if (b.hasMetadata("biab-block") || b.hasMetadata("buildInABox")) {
+            event.setCancelled(true);
+        }
+    }
+
+}
